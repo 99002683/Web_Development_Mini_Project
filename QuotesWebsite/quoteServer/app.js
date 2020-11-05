ed7e22c3f2117a0d695284c5b7d158a856a9f839
99002683 <pooja.pol@ltts.com>
2020-11-05 15:05:49 +0530

    Quotes Website

---
+++ QuotesWebsite/quoteServer/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var quoteList = require('./quotes.json')
+
+
+var cors = function(req, res, next) 
+{ 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    next(); 
+} 
+
+app.use(cors);
+
+//http://localhost:3000/showBooks
+app.get("/showQuotes",function(req,res){
+    res.status(200).send(quoteList);
+})
+app.listen('3000',function(){
+    console.log('Server listning to port 3000');
+   });

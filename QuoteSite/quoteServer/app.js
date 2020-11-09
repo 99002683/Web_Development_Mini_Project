e8e6fe845dfb6771aa6dc46a3697e670b9c2da77
99002658 <subhrosil.maiti@ltts.com>
2020-11-09 16:19:49 +0530

    change__9/11_v1_1

--- QuoteSite/quoteServer/app.js
+++ QuoteSite/quoteServer/app.js
@@ -15,6 +15,29 @@ app.use(cors);
 app.get("/showQuotes",function(req,res){
     res.status(200).send(quoteList);
 })
+
+//http://localhost:3000/showAuthor?author=Kevin Kruse
+app.get("/showAuthor",function(req,res){
+    var author = req.query.author;
+    var newQuoteList =[];
+    quoteList.forEach(quote=>{
+        if(quote.author==author)
+            newQuoteList.push(quote);
+    });
+    res.status(200).send(newQuoteList);
+})
+
+//http://localhost:3000/showCategory?category=Life
+app.get("/showCategory",function(req,res){
+    var category = req.query.category;
+    var newQuoteList =[];
+    quoteList.forEach(quote=>{
+        if(quote.category==category)
+            newQuoteList.push(quote);
+    });
+    res.status(200).send(newQuoteList);
+})
+
 app.listen('3000',function(){
     console.log('Server listning to port 3000');
-   });
+})
